b33311e55da64dbe9d089b5637f7bc81bc9ea88c
Alex Bowyer <alex.bowyer@gmail.com>
2016-02-19 11:23:29 +0000

    Some updates I made ages ago but didn't commit.

--- subject-extractor.py
+++ subject-extractor.py
@@ -1,12 +1,52 @@
 import pymongo
 import urllib
 import csv
+import pickle
+import os
+
 from bson.objectid import ObjectId
 import os
 from sys import argv
 
 DB_NAME = "serengeti"
 
+def nice_seasons():
+  season_chars = list(seasons_string)
+  return ", ".join(season_chars)
+
+def load_all_subjects():
+  if not os.path.exists("pickles"):
+    os.makedirs("pickles")
+  subject_filename = "pickles/%s-subjects.p" % seasons_string
+  if os.path.isfile(subject_filename):
+    print "Loading subject summaries for seasons %s from file..." % nice_seasons()
+    return pickle.load( open( subject_filename, "rb" ) )
+  else:
+    subject_summaries = []
+    client = pymongo.MongoClient()
+    db = client[DB_NAME]
+    subject_collection = db[DB_NAME + "_subjects"]
+    print "Connecting to DB and extracting subject summaries for all subjects in seasons %s...\n" % nice_seasons()
+    for ii, s in enumerate(subject_collection.find({"$or": season_or_list}, no_cursor_timeout=True)):
+      if ii % 1000 == 0:
+        print "%s..." % ii
+      zooniverse_id = s["zooniverse_id"]
+      urls = s["location"]["standard"]
+      site_roll_code = s["metadata"]["site_roll_code"]
+      if "retire_reason" in s["metadata"].keys():
+        retire_reason = s["metadata"]["retire_reason"]
+      else:
+        retire_reason = "N/A"
+      subject_summary = {
+        "zooniverse_id": zooniverse_id,
+        "urls": urls,
+        "site_roll_code": site_roll_code,
+        "retire_reason": retire_reason
+      }
+      subject_summaries.append(subject_summary)
+    print "Saving subject summaries to avoid querying next time..."
+    pickle.dump( subject_summaries, open( subject_filename, "wb" ) )
+    return subject_summaries
 
 def create_csv(csv_directory_name, csv_filename):
   if not os.path.exists(csv_directory_name):
@@ -19,14 +59,17 @@ def create_csv(csv_directory_name, csv_filename):
 
 def add_images_to_csv_for(subject, csvwriter):
   global task_counter_so_far, downloaded_images_so_far, downloaded_subjects_so_far, skipped_images_so_far, skipped_subjects_so_far, csved_images_so_far, csved_subjects_so_far,included_subjects_so_far,included_images_so_far
-  urls = subject["location"]["standard"]
+  urls = subject["urls"]
   csved_this_time = False
   subject_metadata = subjects_index[subject["zooniverse_id"]]
-  site_roll_code = subject["metadata"]["site_roll_code"]
-  retire_reason = subject["metadata"]["retire_reason"]
+  site_roll_code = subject["site_roll_code"]
+  retire_reason = subject["retire_reason"]
   season = subject_metadata["season"]
   type = subject_metadata["type"]
-  multi = ';'.join(subject_metadata["species-list"])
+  if subject_metadata["species-list"] == "N/A":
+    multi="N/A"
+  else:
+    multi = ';'.join(subject_metadata["species-list"])
   frame_no = 0
   for this_url in urls:
     csvwriter.writerow([this_url,subject["zooniverse_id"],frame_no,season,site_roll_code,retire_reason,type,multi])
@@ -42,7 +85,7 @@ def download_images_for(subject, destination_directory_name):
   global task_counter_so_far, downloaded_images_so_far, downloaded_subjects_so_far, skipped_images_so_far, skipped_subjects_so_far,included_subjects_so_far,included_images_so_far
   if not os.path.exists(destination_directory_name):
     os.makedirs(destination_directory_name)
-  urls = subject["location"]["standard"]
+  urls = subject["urls"]
   slash_indices = [i.rfind("/") for i in urls]
   filenames = [str(i[j + 1:]) for i, j in zip(urls, slash_indices)]
   frame_no = 0
@@ -52,7 +95,7 @@ def download_images_for(subject, destination_directory_name):
     to_get = this_url
     to_save = "%s/%s_%s.jpg" % (destination_directory_name, subject["zooniverse_id"], str(frame_no))
     if not (os.path.isfile(to_save)):
-      print "[subject %s frame %s (row %s)] Saving %s to %s..." % (included_subjects_so_far, included_images_so_far, task_counter_so_far, to_get, to_save)
+      print "[subject %s frame %s] Saving %s to %s..." % (included_subjects_so_far, frame_no, to_get, to_save)
       urllib.urlretrieve(to_get, to_save)
       downloaded_images_so_far += 1
       downloaded_this_time = True
@@ -126,12 +169,6 @@ with open('consensus-detailed.csv', 'rb') as csvfile:
 # print "ASG00000n9",subjects_index["ASG00000n9"]
 # print ""
 
-print "Connecting to DB and processing subjects...\n"
-
-client = pymongo.MongoClient()
-db = client[DB_NAME]
-classification_collection = db[DB_NAME + "_classifications"]
-subject_collection = db[DB_NAME + "_subjects"]
 task_counter_so_far = 1
 downloaded_images_so_far = 0
 skipped_images_so_far = 0
@@ -151,23 +188,27 @@ if generate_csv:
 
 matched_so_far = 0
 
-for ii, s in enumerate(subject_collection.find({"$or": season_or_list}, no_cursor_timeout=True)):
+subject_summaries = load_all_subjects()
+
+print "Processing subject summary metadata...\n"
+
+for subject in subject_summaries:
   if limit > 0 and matched_so_far == limit:
     break
-  this_type = subjects_index[s["zooniverse_id"]]["type"]
+  this_type = subjects_index[subject["zooniverse_id"]]["type"]
   if type=="all" or this_type == type:
     matched_so_far += 1
     included_subjects_so_far += 1
     # proceed to write the row
     if download:
       downloaded_images_before = downloaded_images_so_far
-      download_images_for(s, set_name)
+      download_images_for(subject, set_name)
       newly_downloaded_images = (downloaded_images_so_far - downloaded_images_before)
       newly_csved_images = 0
       included_this_time = True
     if generate_csv:
       csved_images_before = csved_images_so_far
-      add_images_to_csv_for(s, csvwriter["writer"])
+      add_images_to_csv_for(subject, csvwriter["writer"])
       newly_csved_images = (csved_images_so_far - csved_images_before)
       newly_downloaded_images = 0
     newly_included_images = max(newly_csved_images,newly_downloaded_images)
